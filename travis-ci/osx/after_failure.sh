900d1273170f6c2121e4f7e66f7e0c1f6f55742d
Christian Schenk <cs@miktex.org>
2018-06-23 21:05:02 +0200

    Use Travis-CI

--- travis-ci/osx/after_failure.sh
+++ travis-ci/osx/after_failure.sh
@@ -8,6 +8,6 @@ if [ -d "$miktex_home/texmfs/data/miktex/log" ]; then
     (cd "$miktex_home/texmfs/data/miktex/log"; cat *)
 fi
 
-if [ -f "${TRAVIS_BUILD_DIR}/miktex-testing/build/Testing/Temporary/LastTest.log" ]; then
-    cat "${TRAVIS_BUILD_DIR}/miktex-testing/build/Testing/Temporary/LastTest.log"
+if [ -f "${TRAVIS_BUILD_DIR}/build/Testing/Temporary/LastTest.log" ]; then
+    cat "${TRAVIS_BUILD_DIR}/build/Testing/Temporary/LastTest.log"
 fi
--- travis-ci/osx/script.sh
+++ travis-ci/osx/script.sh
@@ -3,31 +3,12 @@
 set -e
 set -v
 
-curl --location --output miktex-2.9.6715-1-darwin-x86_64.dmg http://download.miktex.org/tmp/6A8D5E99E29F48209C541EFAFD88F0C5/miktex-2.9.6715-1-darwin-x86_64.dmg
-echo Y | hdiutil attach miktex-2.9.6715-1-darwin-x86_64.dmg
-ls -l /Volumes/miktex-2.9.6715-1-darwin-x86_64
+version=2.9.6715
 
+curl --location --output miktex-$version-1-darwin-x86_64.dmg https://miktex.org/download/ctan/systems/win32/miktex/setup/darwin-x86_64/miktex-$version-1-darwin-x86_64.dmg
+echo Y | hdiutil attach miktex-$version-1-darwin-x86_64.dmg
 
-miktex_bin="/Volumes/miktex-2.9.6715-1-darwin-x86_64/MiKTeX Console.app/Contents/bin"
-miktex_home="$HOME/Library/Application Support/MiKTeX"
-
-sudomiktex() {
-    set +e
-    sudo MIKTEX_SHAREDSETUP=t "$miktex_bin"/$1 $2 $3 $4 $5 $6 $7 $8 $9
-    local status=$?
-    if [ $status -ne 0 ]; then
-	if [ -d "$miktex_home/texmfs/data/miktex/log" ]; then
-	    cd "$miktex_home/texmfs/data/miktex/log"
-	    grep FATAL *
-	fi
-	if [ -d "/var/log/miktex" ]; then
-	    cd "/var/log/miktex"
-	    grep FATAL *
-	fi
-    fi
-    set -e
-    return $status
-}
+miktex_bin="/Volumes/miktex-$version-1-darwin-x86_64/MiKTeX Console.app/Contents/bin"
 
 "${miktex_bin}/miktexsetup" finish
 "${miktex_bin}/initexmf" --set-config-value=[MPM]AutoInstall=1
@@ -39,5 +20,4 @@ cd build
 
 cmake -DMIKTEX_BINARY_DIR="$miktex_bin" ..
 
-#export MIKTEX_TRACE=error
 make test
