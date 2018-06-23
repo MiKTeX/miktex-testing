2a7b5a5c770a298790a0a260b688e96f539f3138
Christian Schenk <cs@miktex.org>
2018-06-23 12:11:32 +0200

    Use Travis-CI

--- travis-ci/osx/after_failure.sh
+++ travis-ci/osx/after_failure.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
 
 set -v
+
+miktex_home="$HOME/Library/Application Support/MiKTeX"
+
+if [ -d "$miktex_home/texmfs/data/miktex/log" ]; then
+    (cd "$miktex_home/texmfs/data/miktex/log"; cat *)
+fi
+
+if [ -f "${TRAVIS_BUILD_DIR}/miktex-testing/build/Testing/Temporary/LastTest.log" ]; then
+    cat "${TRAVIS_BUILD_DIR}/miktex-testing/build/Testing/Temporary/LastTest.log"
+fi
