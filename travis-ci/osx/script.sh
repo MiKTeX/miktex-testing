d2679bc3e02bc7f403dd14226f3d00e1e5b00401
Christian Schenk <cs@miktex.org>
2018-08-27 11:38:08 +0200

    Use Travis-CI

--- travis-ci/osx/script.sh
+++ travis-ci/osx/script.sh
@@ -8,13 +8,14 @@ url=http://miktex.org/download/mac/miktex-x64.dmg
 curl --insecure --location --output miktex.dmg $url
 echo Y | hdiutil attach miktex.dmg
 
-MIKTEX_VERSION=2.9.6800
+MIKTEX_MILESTONE=6800
 
-contents="/volumes/miktex-$MIKTEX_VERSION-1-darwin-x86_64/MiKTeX Console.app/Contents"
+contents="/volumes/miktex-2.9.$MIKTEX_MILESTONE-1-darwin-x86_64/MiKTeX Console.app/Contents"
 miktex_bin="$contents/bin"
 
 "${miktex_bin}/miktexsetup" finish
 "${miktex_bin}/initexmf" --set-config-value=[MPM]AutoInstall=1
+"${miktex_bin}/initexmf" --set-config-value=[MPM]RemoteService_$MIKTEX_MILESTONE=multiplexor
 "${miktex_bin}/mpm" --package-level=basic --upgrade
 
 "$contents/MacOS/MiKTeX Console" --version
