3bdca98af1bc8c1b3974455d5518c38afbf08495
Christian Schenk <cs@miktex.org>
2020-02-04 23:34:39 +0100

    Travis-CI: macOS

--- travis-ci/osx/before_script.sh
+++ travis-ci/osx/before_script.sh
@@ -2,10 +2,3 @@
 
 set -e
 set -v
-
-brew update
-
-#brew outdated cmake || brew upgrade cmake
-
-( brew ls -1 | grep -w ghostscript ) || brew install ghostscript
-( brew ls -1 | grep -w md5sha1sum ) || brew install md5sha1sum
